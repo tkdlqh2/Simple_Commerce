8254d86c16767ac5ac9e69559e082624c5fd9920
tkdlqh2 <yhj7124@naver.com>
2022-10-05 17:27:43 +0900

    fix: 기존 에러 발생 코드 수정

--- src/main/java/com/zerobase/cms/zerobasecms/application/SignUpApplication.java
+++ src/main/java/com/zerobase/cms/zerobasecms/application/SignUpApplication.java
@@ -3,6 +3,8 @@ package com.zerobase.cms.zerobasecms.application;
 import com.zerobase.cms.zerobasecms.client.MailgunClient;
 import com.zerobase.cms.zerobasecms.domain.SignUpForm;
 import com.zerobase.cms.zerobasecms.domain.model.Customer;
+import com.zerobase.cms.zerobasecms.exception.CustomException;
+import com.zerobase.cms.zerobasecms.exception.ErrorCode;
 import com.zerobase.cms.zerobasecms.service.SignUpCustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +17,7 @@ public class SignUpApplication {
 
     public void customerSignUp(SignUpForm form){
         if(signUpCustomerService.isEmailExist(form.getEmail())){
-            throw new CustomerException();
+            throw new CustomException(ErrorCode.ALREADY_REGISTERED_USER);
         }else{
             Customer c= signUpCustomerService.signUp(form);
         }
