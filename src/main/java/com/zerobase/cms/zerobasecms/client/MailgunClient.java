70b9bb98800e66d7dbc08ff87c406e30a3da3799
tkdlqh2 <yhj7124@naver.com>
2022-10-05 16:53:34 +0900

    feat: [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요

---
+++ src/main/java/com/zerobase/cms/zerobasecms/client/MailgunClient.java
@@ -0,0 +1,18 @@
+package com.zerobase.cms.zerobasecms.client;
+
+import com.zerobase.cms.zerobasecms.client.mailgun.SendMailForm;
+import feign.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "mailgun", url="https://api.mailgun/v3/")
+@Qualifier("mailgun")
+public interface MailgunClient {
+
+    @PostMapping("sandbox3c671fcf2b8640e29f23318c0d9aa37f.mailgun.org/messages")
+    ResponseEntity<Response> sendEmail(@SpringQueryMap SendMailForm form);
+
+}
