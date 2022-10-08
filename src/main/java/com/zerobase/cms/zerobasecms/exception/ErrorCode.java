e8dbc756de4158fd73d91ae3ad02bdfffd6229f2
tkdlqh2 <yhj7124@naver.com>
2022-10-08 15:02:56 +0900

    add: changeCustomerValidateEmail 추가

--- src/main/java/com/zerobase/cms/zerobasecms/exception/ErrorCode.java
+++ src/main/java/com/zerobase/cms/zerobasecms/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
+    UNREGISTERED_USER(HttpStatus.BAD_REQUEST,"회원 정보를 찾을 수 없습니다."),
     ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST,"이미 가입된 회원입니다.");
 
     private final HttpStatus httpStatus;
--- src/main/java/com/zerobase/cms/zerobasecms/service/SignUpCustomerService.java
+++ src/main/java/com/zerobase/cms/zerobasecms/service/SignUpCustomerService.java
@@ -3,10 +3,14 @@ package com.zerobase.cms.zerobasecms.service;
 import com.zerobase.cms.zerobasecms.domain.CustomerRepository;
 import com.zerobase.cms.zerobasecms.domain.SignUpForm;
 import com.zerobase.cms.zerobasecms.domain.model.Customer;
+import com.zerobase.cms.zerobasecms.exception.CustomException;
+import com.zerobase.cms.zerobasecms.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
 
 @Service
@@ -25,5 +29,15 @@ public class SignUpCustomerService {
 
     public boolean isEmailExist(String email){
         return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT)).isPresent();
+    }
+
+    @Transactional
+    public void changeCustomerValidateEmail(Long customerId,String verificationCode){
+        Customer customer = customerRepository.findById(customerId).orElseThrow(
+                ()-> new CustomException(ErrorCode.UNREGISTERED_USER)
+        );
+
+        customer.setVerificationCode(verificationCode);
+        customer.setVerifyExpiredAt(LocalDateTime.now());
     }
 }
