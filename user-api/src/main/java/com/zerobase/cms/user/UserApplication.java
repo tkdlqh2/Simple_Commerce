9d6b0d5e56b8780df13de5f4644ea499ef879582
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-22 16:51:20 +0900

    feat : annotation 변경

--- user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
@@ -1,11 +1,19 @@
 package com.zerobase.cms.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableFeignClients
+@ServletComponentScan
+@EnableJpaAuditing
+@RequiredArgsConstructor
+@EnableSwagger2
 @SpringBootApplication
+@EnableFeignClients
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
