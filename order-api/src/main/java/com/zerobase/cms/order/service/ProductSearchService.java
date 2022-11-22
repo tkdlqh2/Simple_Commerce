1711ea613a967b85decf93d3c25dd2c6b9b959a3
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-23 00:11:41 +0900

    feat : SearchController 에 searchByName API 추가

--- order-api/src/main/java/com/zerobase/cms/order/service/ProductSearchService.java
+++ order-api/src/main/java/com/zerobase/cms/order/service/ProductSearchService.java
@@ -15,6 +15,10 @@ import static com.zerobase.cms.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
 public class ProductSearchService {
     private final ProductRepository productRepository;
 
+    public List<Product> searchByName(String name){
+        return productRepository.searchByName(name);
+    }
+
     public Product getByProductId(Long productId){
         return productRepository.findWithProductItemsById(productId)
                 .orElseThrow(()-> new CustomException(NOT_FOUND_PRODUCT));
