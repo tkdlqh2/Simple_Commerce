52b114945cd622cff324244f824c9ac00667b0c0
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-28 21:16:55 +0900

    feat : Order API 추가

--- order-api/src/main/java/com/zerobase/cms/order/client/UserClient.java
+++ order-api/src/main/java/com/zerobase/cms/order/client/UserClient.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "user-api", url = "${feign.client.url.user-api}")
 public interface UserClient {
 
-    @GetMapping("/getInfo")
+    @GetMapping("/customer/getInfo")
     ResponseEntity<CustomerDto> getCustomerInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token);
 
-    @PostMapping("/balance")
+    @PostMapping("/customer/balance")
     ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token,
                                           @RequestBody ChangeBalanceForm form);
 }
