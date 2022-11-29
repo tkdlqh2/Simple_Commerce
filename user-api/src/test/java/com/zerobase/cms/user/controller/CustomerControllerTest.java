012e7f6dbebdc55c94cad30f026bf92a473fad0c
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-30 02:54:05 +0900

    test: NotEnoughBalance test 추가

--- user-api/src/test/java/com/zerobase/cms/user/controller/CustomerControllerTest.java
+++ user-api/src/test/java/com/zerobase/cms/user/controller/CustomerControllerTest.java
@@ -1,7 +1,10 @@
 package com.zerobase.cms.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.cms.user.domain.model.Customer;
+import com.zerobase.cms.user.domain.model.CustomerBalanceHistory;
+import com.zerobase.cms.user.exception.CustomException;
 import com.zerobase.cms.user.service.customer.CustomerBalanceService;
 import com.zerobase.cms.user.service.customer.CustomerService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
@@ -13,14 +16,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
+import static com.zerobase.cms.user.exception.ErrorCode.NOT_ENOUGH_BALANCE;
 import static com.zerobase.cms.user.exception.ErrorCode.UNREGISTERED_USER;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,4 +99,61 @@ class CustomerControllerTest {
                 .andDo(print());
     }
 
+    @Test
+    void changeBalanceSuccess() throws Exception {
+        //given
+
+        ChangeBalanceForm form = new ChangeBalanceForm("from","message",20000);
+        given(customerBalanceService.changeBalance(eq(USER_ID),any()))
+                .willReturn(CustomerBalanceHistory.builder()
+                        .currentMoney(25000)
+                        .build());
+        //when
+        //then
+        mockMvc.perform(post("/customer/balance")
+                        .header("X-AUTH-TOKEN","")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(25000))
+                .andDo(print());
+    }
+
+    @Test
+    void changeBalanceFail_NoCustomer() throws Exception {
+        //given
+
+        ChangeBalanceForm form = new ChangeBalanceForm("from","message",20000);
+        doThrow(new CustomException(UNREGISTERED_USER))
+                .when(customerBalanceService).changeBalance(eq(USER_ID),any());
+
+        //when
+        //then
+        mockMvc.perform(post("/customer/balance")
+                        .header("X-AUTH-TOKEN","")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(UNREGISTERED_USER.getDetail()))
+                .andDo(print());
+    }
+
+    @Test
+    void changeBalanceFail_NotEnoughBalance() throws Exception {
+        //given
+
+        ChangeBalanceForm form = new ChangeBalanceForm("from","message",20000);
+        doThrow(new CustomException(NOT_ENOUGH_BALANCE))
+                .when(customerBalanceService).changeBalance(eq(USER_ID),any());
+
+        //when
+        //then
+        mockMvc.perform(post("/customer/balance")
+                        .header("X-AUTH-TOKEN","")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(NOT_ENOUGH_BALANCE.getDetail()))
+                .andDo(print());
+    }
 }
