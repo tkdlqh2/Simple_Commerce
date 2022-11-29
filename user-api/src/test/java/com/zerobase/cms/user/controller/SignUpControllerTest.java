64c97e979ca67bc5c35c2dacb8785f3d0bcd2c9e
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-30 03:36:14 +0900

    test : SignUpControllerTest class 추가 및 customerSignUp test 추가

---
+++ user-api/src/test/java/com/zerobase/cms/user/controller/SignUpControllerTest.java
@@ -0,0 +1,105 @@
+package com.zerobase.cms.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.cms.user.application.SignUpApplication;
+import com.zerobase.cms.user.domain.SignUpForm;
+import com.zerobase.cms.user.exception.CustomException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static com.zerobase.cms.user.exception.ErrorCode.ALREADY_REGISTERED_USER;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(SignUpController.class)
+@AutoConfigureDataJpa
+@AutoConfigureMockMvc(addFilters = false)
+class SignUpControllerTest {
+
+    @MockBean
+    private SignUpApplication signUpApplication;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void customerSignUpSuccess() throws Exception {
+        //given
+        SignUpForm form = SignUpForm.builder()
+                .email("abc@gmail.com")
+                .name("홍길동")
+                .password("abcd2@ef")
+                .birth(LocalDate.of(1990,1,1))
+                .build();
+
+        given(signUpApplication.customerSignUp(any()))
+                .willReturn("");
+        //when
+        //then
+        mockMvc.perform(post("/signup/customer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("가입 신청이 완료되었습니다."))
+                .andDo(print());
+    }
+
+    @Test
+    void customerSignUpFail_ArgumentException() throws Exception {
+        //given
+
+        SignUpForm form = SignUpForm.builder()
+                .email("abc@gmail.com")
+                .name("홍길동")
+                .password("abcd")
+                .birth(LocalDate.of(1990,1,1))
+                .build();
+        //when
+        //then
+        mockMvc.perform(post("/signup/customer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andDo(print());
+    }
+
+    @Test
+    void customerSignUpFail_AlreadyRegisteredUser() throws Exception {
+        //given
+        SignUpForm form = SignUpForm.builder()
+                .email("abc@gmail.com")
+                .name("홍길동")
+                .password("abcd2@ef")
+                .birth(LocalDate.of(1990,1,1))
+                .build();
+
+        doThrow(new CustomException(ALREADY_REGISTERED_USER))
+                .when(signUpApplication).customerSignUp(any());
+        //when
+        //then
+        mockMvc.perform(post("/signup/customer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(form)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ALREADY_REGISTERED_USER.getDetail()))
+                .andDo(print());
+    }
+}
